2b115d263737efcac54df2658be311040932919c
Amal25004 <jmaldonado250@icloud.com>
2024-10-20 10:35:01 -0500

    added .env and firebase API_KEY

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+require('dotenv').config();
+const APIKEY = process.env.firebase_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0wvPBwjJVegw2nyGbGLVPIPRSBNfcZl4",
+  apiKey: APIKEY,
   authDomain: "jobsync-6e749.firebaseapp.com",
   projectId: "jobsync-6e749",
   storageBucket: "jobsync-6e749.appspot.com",
@@ -19,3 +21,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
